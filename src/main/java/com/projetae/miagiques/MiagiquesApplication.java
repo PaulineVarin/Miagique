b6c7ecd3a3d8a6d6c366314aca594cfbf2b412da
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-03-25 14:46:04 +0100

    First commit

---
+++ src/main/java/com/projetae/miagiques/MiagiquesApplication.java
@@ -0,0 +1,13 @@
+package com.projetae.miagiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiagiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiagiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetae/miagiques/MiagiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetae.miagiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiagiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
