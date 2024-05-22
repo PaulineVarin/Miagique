ee04fcf134cf836a253242ba034477545acde59e
Morgan Nayet <81689443+Aaravoss@users.noreply.github.com>
2024-05-22 14:42:31 +0200

    ajout des OneToMany ManyToOne

--- src/main/java/com/projetae/miagiques/entities/Spectateur.java
+++ src/main/java/com/projetae/miagiques/entities/Spectateur.java
@@ -1,10 +1,13 @@
 package com.projetae.miagiques.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
 
 @Entity
 @AllArgsConstructor
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Spectateur extends Personne {
+
+    @OneToMany(mappedBy = "spectateur")
+    private ArrayList<Billet> billets;
 }
