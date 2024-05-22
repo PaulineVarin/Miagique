7d8a9a17bcd08a6578722b6ea3543f8c1348e13c
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-05-22 14:43:50 +0200

    fix: finish Billet + Epreuve + Spectateur

--- src/main/java/com/projetae/miagiques/entities/Spectateur.java
+++ src/main/java/com/projetae/miagiques/entities/Spectateur.java
@@ -1,4 +1,12 @@
 package com.projetae.miagiques.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+
+@Entity
 public class Spectateur extends Personne {
+    @OneToMany(mappedBy = 'spectateur')
+    private ArrayList<Billet> billets ;
 }
