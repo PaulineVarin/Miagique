302abf8e700460c4dd5c6cd55ca454eddc86b993
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-05-22 15:22:29 +0200

    fix: adding all controllers

---
+++ src/main/java/com/projetae/miagiques/exposition/DelegationController.java
@@ -0,0 +1,9 @@
+package com.projetae.miagiques.exposition;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class DelegationController {
+}
