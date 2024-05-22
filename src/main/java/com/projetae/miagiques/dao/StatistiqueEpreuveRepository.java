a5a44db7fbdf9f003c1c5ed17b4276ee9b096e25
Morgan Nayet <81689443+Aaravoss@users.noreply.github.com>
2024-05-22 15:31:44 +0200

    ajout des repos manquants

---
+++ src/main/java/com/projetae/miagiques/dao/StatistiqueEpreuveRepository.java
@@ -0,0 +1,7 @@
+package com.projetae.miagiques.dao;
+
+import com.projetae.miagiques.entities.StatistiqueEpreuve;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StatistiqueEpreuveRepository extends CrudRepository<StatistiqueEpreuve, Long> {
+}
