d6b46d4a0c96f410651430ebee4c33cc5ea8b055
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-05-23 13:29:01 +0200

    fix: adding sql file to initiate database if needeed

--- src/main/java/com/projetae/miagiques/metier/SpectateurService.java
+++ src/main/java/com/projetae/miagiques/metier/SpectateurService.java
@@ -1,7 +1,23 @@
 package com.projetae.miagiques.metier;
 
+import com.projetae.miagiques.dao.SpectateurRepository;
+import com.projetae.miagiques.entities.Billet;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 public class SpectateurService {
+    private final SpectateurRepository spectateurRepository ;
+
+    public SpectateurService(SpectateurRepository spectateurRepository) {
+        this.spectateurRepository = spectateurRepository;
+    }
+
+    public Collection<Billet> getBilletsSpectateur(Long idSpectateur) {
+        this.spectateurRepository.findById()
+    }
+
+
 }
