4b1eee53b2ceb01b05af24c5b3d9d0805bb1f773
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-05-23 15:44:31 +0200

    fix: reply to commentaires

--- src/main/java/com/projetae/miagiques/exposition/SpectateurController.java
+++ src/main/java/com/projetae/miagiques/exposition/SpectateurController.java
@@ -1,10 +1,13 @@
 package com.projetae.miagiques.exposition;
 
+import com.projetae.miagiques.entities.Billet;
 import com.projetae.miagiques.metier.SpectateurService;
 import com.projetae.miagiques.utilities.BilletExceptions.BilletAnnulationImpossible;
 import com.projetae.miagiques.utilities.BilletExceptions.BilletInexistant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/spectateurs")
@@ -15,7 +18,10 @@ public class SpectateurController {
     //necessaire de verifier avant chaque appel de methode si l'utilisateur est connecté
     //on utilise l'adresse email dans l'uri pour faire ça
 
-
+    @GetMapping({"{id_spectateur}/listerBillets"})
+    public Collection<Billet> getAllBillets(@PathVariable("id_spectateur") Long idSpectateur) {
+        return this.spectateurService.getAllBillets(idSpectateur) ;
+    }
     @PutMapping("/{id_spectateur}/annulerBillet/{id_billet}")
     public String annulerBillet(@PathVariable("id_spectateur") Long idSpectateur,@PathVariable("id_billet") Long idBillet) throws BilletInexistant, BilletAnnulationImpossible {
         return this.spectateurService.annulerBillet(idBillet, idSpectateur);
--- src/main/java/com/projetae/miagiques/metier/SpectateurService.java
+++ src/main/java/com/projetae/miagiques/metier/SpectateurService.java
@@ -1,7 +1,6 @@
 package com.projetae.miagiques.metier;
 
 import com.projetae.miagiques.dao.BilletRepository;
-import com.projetae.miagiques.dao.SpectateurRepository;
 import com.projetae.miagiques.entities.Billet;
 import com.projetae.miagiques.entities.Epreuve;
 import com.projetae.miagiques.utilities.BilletExceptions.BilletAnnulationImpossible;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.Duration;
+import java.util.Collection;
 
 import static java.util.Objects.isNull;
 
@@ -23,34 +23,36 @@ public class SpectateurService {
         this.billetRepository = billetRepository;
     }
 
+    public Collection<Billet> getAllBillets(Long idSpectateur) {
+        return this.billetRepository.findAllBySpectateurId(idSpectateur) ;
+    }
+
     public String annulerBillet(Long idBillet, Long idSpectateur) throws BilletInexistant, BilletAnnulationImpossible {
-        Billet bi = this.billetRepository.findByIdBilletIsAndSpectateurId(idBillet, idSpectateur) ;
-        if(isNull(bi)) {
-            throw new BilletInexistant() ;
-        } else {
-            Epreuve e = bi.getEpreuve() ;
-            Timestamp dateEpreuve = e.getDate() ;
-            Timestamp dateCourante = new Timestamp(System.currentTimeMillis()) ;
-            Long differenceJours = Duration.between(dateCourante.toLocalDateTime(),dateEpreuve.toLocalDateTime()).toDays() ;
-
-            if(differenceJours < 3) {
-                throw new BilletAnnulationImpossible() ;
-            }
-            else {
-                bi.setEtat(StatutBillet.ANNULE);
-                this.billetRepository.save(bi) ;
-                float montantRembourse = 0 ;
-
-                if(differenceJours > 7 ) {
-                    montantRembourse = bi.getPrix() ;
-                }
-                else {
-                    montantRembourse = (float) (bi.getPrix() * 0.5);
-                }
-
-                String m = "Opération de remboursement réussi : " + montantRembourse ;
-                return m ;
-            }
+        Billet bi = this.billetRepository.findByIdBilletIsAndSpectateurId(idBillet, idSpectateur);
+        if (isNull(bi)) {
+            throw new BilletInexistant();
         }
+
+        Epreuve e = bi.getEpreuve();
+        Timestamp dateEpreuve = e.getDate();
+        Timestamp dateCourante = new Timestamp(System.currentTimeMillis());
+        Long differenceJours = Duration.between(dateCourante.toLocalDateTime(), dateEpreuve.toLocalDateTime()).toDays();
+
+        if (differenceJours < 3) {
+            throw new BilletAnnulationImpossible();
+        }
+
+        bi.setEtat(StatutBillet.ANNULE);
+        this.billetRepository.save(bi);
+        float montantRembourse = 0;
+
+        if (differenceJours > 7) {
+            montantRembourse = bi.getPrix();
+        } else {
+            montantRembourse = bi.getPrix() * 0.5f;
+        }
+
+        String m = "Opération de remboursement réussi : " + montantRembourse;
+        return m;
     }
 }
