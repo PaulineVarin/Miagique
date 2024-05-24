1644b69d16f7b4602ff6a5850cbc2793dfbf97d9
Morgan Nayet <81689443+Aaravoss@users.noreply.github.com>
2024-05-24 13:40:45 +0200

    Ajout Récupération de toutes les épreuves

--- src/main/java/com/projetae/miagiques/exposition/EpreuveController.java
+++ src/main/java/com/projetae/miagiques/exposition/EpreuveController.java
@@ -1,9 +1,25 @@
 package com.projetae.miagiques.exposition;
 
+import com.projetae.miagiques.entities.Epreuve;
+import com.projetae.miagiques.metier.EpreuveService;
+import com.projetae.miagiques.metier.SpectateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.List;
+
 @RestController
-@RequestMapping()
+@RequestMapping("/epreuves")
 public class EpreuveController {
+
+    @Autowired
+    private EpreuveService epreuveService;
+
+    @GetMapping("/")
+    public Collection<Epreuve> getAllEpreuves(){
+        return this.epreuveService.getAll();
+    }
 }
--- src/main/java/com/projetae/miagiques/metier/EpreuveService.java
+++ src/main/java/com/projetae/miagiques/metier/EpreuveService.java
@@ -1,7 +1,22 @@
 package com.projetae.miagiques.metier;
 
+import com.projetae.miagiques.dao.EpreuveRepository;
+import com.projetae.miagiques.entities.Epreuve;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class EpreuveService {
+
+    private final EpreuveRepository epreuveRepository;
+
+    public EpreuveService(EpreuveRepository epreuveRepository) {
+        this.epreuveRepository = epreuveRepository;
+    }
+
+    public Collection<Epreuve> getAll(){
+
+        return (Collection<Epreuve>) this.epreuveRepository.findAll();
+    }
 }
