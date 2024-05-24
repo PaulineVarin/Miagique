122aa8d9abf888247b2179a3ae362b4eb142f244
Morgan Nayet <81689443+Aaravoss@users.noreply.github.com>
2024-05-24 10:00:23 +0200

    modification des exceptions pour prendre en compte les status HTTP

--- src/main/java/com/projetae/miagiques/utilities/BilletExceptions/BilletInexistant.java
+++ src/main/java/com/projetae/miagiques/utilities/BilletExceptions/BilletInexistant.java
@@ -1,4 +1,20 @@
 package com.projetae.miagiques.utilities.BilletExceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class BilletInexistant extends Exception{
+
+    private HttpStatus httpStatus;
+
+    public BilletInexistant(){
+        this.httpStatus = HttpStatus.CONFLICT;
+    };
+
+    public BilletInexistant(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getStatus(){
+        return this.httpStatus;
+    }
 }
