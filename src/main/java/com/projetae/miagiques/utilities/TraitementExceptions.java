6d9204bfe3938e29fe20f337c6604603533034ce
Morgan Nayet <81689443+Aaravoss@users.noreply.github.com>
2024-05-29 08:11:29 +0200

    création exception role not allow

--- src/main/java/com/projetae/miagiques/utilities/TraitementExceptions.java
+++ src/main/java/com/projetae/miagiques/utilities/TraitementExceptions.java
@@ -2,6 +2,7 @@ package com.projetae.miagiques.utilities;
 
 import com.projetae.miagiques.utilities.BilletExceptions.BilletAnnulationImpossible;
 import com.projetae.miagiques.utilities.BilletExceptions.BilletInexistant;
+import com.projetae.miagiques.utilities.PersonneExceptions.RoleNotAllowException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class TraitementExceptionsBillet {
+public class TraitementExceptions {
     @ExceptionHandler(BilletInexistant.class)
     public ResponseEntity<String> gererBilletInexistant (HttpServletRequest requete, BilletInexistant ex) {
         return new ResponseEntity<>("Ce billet n'est pas trouvable dans la base", ex.getStatus()) ;
@@ -18,5 +19,10 @@ public class TraitementExceptionsBillet {
     @ExceptionHandler(BilletAnnulationImpossible.class)
     public ResponseEntity<String> gererBilletAnnulationImpossible (HttpServletRequest requete, BilletAnnulationImpossible ex) {
         return new ResponseEntity<>("Impossible d'annuler ce billet", HttpStatus.BAD_REQUEST) ;
+    }
+
+    @ExceptionHandler(RoleNotAllowException.class)
+    public ResponseEntity<String> gererRoleNotAllowException (HttpServletRequest requete, RoleNotAllowException ex){
+        return new ResponseEntity<>("You are not authorized to access this service", ex.getStatus());
     }
 }
