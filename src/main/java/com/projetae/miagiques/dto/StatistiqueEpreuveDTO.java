61f3e850c632d322567a198ec544657b4c1f4dcf
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-06-19 20:04:33 +0200

    Update StatistiqueEpreuveDTO.java

--- src/main/java/com/projetae/miagiques/dto/StatistiqueEpreuveDTO.java
+++ src/main/java/com/projetae/miagiques/dto/StatistiqueEpreuveDTO.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class StatistiqueEpreuveDTO {
-    private Long idStatistiqueEpreuve;
     private EpreuveDTO epreuve;
     private int nbPlacesRestantes;
     private int nbPlaces;
